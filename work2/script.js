bc2e92c74230868bcd15fa75f3fbd901356a63c5
Roman Izmailoov Owner PC <work69lin@gmail.com>
2025-05-22 12:48:22 +0300

    Работа 2

--- work2/script.js
+++ work2/script.js
@@ -1,30 +1,38 @@
-let fontSize = 16;
 
-        function toggleTextSize() {
-            fontSize = fontSize === 16 ? 20 : 16;
-            document.body.style.fontSize = `${fontSize}px`;
-        }
+  let fontSize = 16;
 
-        const textBlock = document.getElementById('textBlock');
+  function toggleTextSize() {
+    fontSize = fontSize === 16 ? 20 : 16;
+    document.body.style.fontSize = `${fontSize}px`;
+  }
 
-        textBlock.addEventListener('mouseover', () => {
-            textBlock.style.backgroundColor = 'lightgreen';
-            textBlock.style.transform = 'scale(1.1)';
-            textBlock.style.transition = 'all 0.3s ease';
-        });
+  
+  const toggleBtn = document.getElementById('toggleSizeBtn');
+  toggleBtn.addEventListener('click', toggleTextSize);
 
-        textBlock.addEventListener('mouseout', () => {
-            textBlock.style.backgroundColor = 'lightblue';
-            textBlock.style.transform = 'scale(1)';
-        });
+ 
+  const textBlock = document.getElementById('textBlock');
 
-        function validateInput(event) {
-            event.preventDefault();
-            const input = document.getElementById('inputField');
-            if (input.value.trim() === '') {
-                input.classList.add('error');
-            } else {
-                input.classList.remove('error');
-                alert('Данные отправлены!');
-            }
-        }
+  textBlock.addEventListener('mouseover', () => {
+    textBlock.style.backgroundColor = 'lightgreen';
+    textBlock.style.transform = 'scale(1.1)';
+    textBlock.style.transition = 'all 0.3s ease';
+  });
+
+  textBlock.addEventListener('mouseout', () => {
+    textBlock.style.backgroundColor = 'lightblue';
+    textBlock.style.transform = 'scale(1)';
+  });
+
+  // Валидация формы
+  const form = document.querySelector('form');
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const input = document.getElementById('inputField');
+    if (input.value.trim() === '') {
+      input.classList.add('error');
+    } else {
+      input.classList.remove('error');
+      alert('Данные отправлены!');
+    }
+  });
